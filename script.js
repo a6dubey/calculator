d950e679d58d2fdf2ee921f12cd450ff50d6918b
a6dubey <109572601+a6dubey@users.noreply.github.com>
2022-10-08 18:50:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let screen=document.getElementById('screen');
+buttons= document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttonText=e.target.innerText;
+        console.log('Button Text is', buttonText);
+        if(buttonText == '='){
+            screenValue=eval(screenValue);
+            screen.value=screenValue;
+            screenValue='';
+
+        }else
+        if(buttonText == "AC"){
+            screenValue="";
+            screen.value=screenValue;
+
+        }else{
+            screenValue +=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
