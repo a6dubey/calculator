98d00d7543d2b99b19a4da74218734393033900c
a6dubey <109572601+a6dubey@users.noreply.github.com>
2022-10-09 11:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let screen=document.getElementById('screen');
+console.log(screen.value);
 buttons= document.querySelectorAll('button');
 let screenValue='';
+
 for(item of buttons){
     item.addEventListener('click', (e)=>{
         buttonText=e.target.innerText;
-        console.log('Button Text is', buttonText);
+        // console.log('Button Text is', buttonText);
         if(buttonText == '='){
-              try{
+            try{
                 screenValue=eval(screenValue);
                 screen.value=screenValue;
             }catch(err){
-                alert("Upar Wale se to daro BETEEE ")
+                // alert("Upar Wale se to daro BETEEE ")
+                screen.value="BETEEE";
             }
+            
+            // screenValue=screen.value;
 
+        }else
+        if(buttonText== 'DEL'){
+            screen.value=screen.value.slice(0,-1);
         }else
         if(buttonText == "AC"){
             screenValue="";
